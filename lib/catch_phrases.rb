bba22142449506733215c6e53f17c86ce1d89624
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-03 08:20:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,15 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+status="Thank You Mario! But Our Princess Is In Another Castle!"
+
 def toadstool
   puts status
+end
+
+def link
+  phrase="It's Dangerous To Go Alone!Take This."
+  puts phrase
 end
